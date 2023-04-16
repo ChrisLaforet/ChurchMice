1638238a4ded125d8c612e4db781b0551823ffaf
Chris <laforet@chrislaforetsoftware.com>
2023-04-16 17:51:50 -0400

    SQL script update

--- DBScripts/ChurchMiceDB.sql
+++ DBScripts/ChurchMiceDB.sql
@@ -74,3 +74,59 @@ GO
 ALTER TABLE [dbo].[UserToken] CHECK CONSTRAINT [FK_UserToken_User]
 GO
 
+/****** Object:  Table [dbo].[EmailQueue]    Script Date: 4/10/2023 7:24:07 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[EmailQueue](
+	[ID] [varchar](50) NOT NULL,
+	[EmailRecipient] [varchar](255) NOT NULL,
+	[EmailSender] [varchar](255) NOT NULL,
+	[EmailSubject] [varchar](255) NULL,
+	[EmailBody] [varchar](max) NOT NULL,
+	[SentDatetime] [datetime2](7) NOT NULL,
+	[AttemptDatetime] [datetime2](7) NULL,
+	[TotalAttempts] [int] NOT NULL,
+	[AttachmentFilename] [varchar](255) NULL,
+ CONSTRAINT [PK_EmailQueue] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Member]    Script Date: 4/10/2023 7:24:07 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Member](
+	[ID] [int] NOT NULL,
+	[FirstName] [varchar](30) NOT NULL,
+	[LastName] [varchar](30) NOT NULL,
+	[Email] [varchar](255) NULL,
+	[HomePhone] [varchar](20) NULL,
+	[MobilePhone] [varchar](20) NULL,
+	[MailingAddress1] [varchar](100) NULL,
+	[MailingAddress2] [varchar](100) NULL,
+	[City] [varchar](50) NULL,
+	[State] [varchar](2) NULL,
+	[Zip] [varchar](10) NULL,
+	[Birthday] [varchar](20) NULL,
+	[Anniversary] [varchar](20) NULL,
+	[MemberSince] [date] NULL,
+	[Created] [datetime2](7) NOT NULL,
+	[UserID] [varchar](50) NULL,
+ CONSTRAINT [PK_Member] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Member]  WITH CHECK ADD  CONSTRAINT [FK_Member_User] FOREIGN KEY([UserID])
+REFERENCES [dbo].[User] ([ID])
+GO
+ALTER TABLE [dbo].[Member] CHECK CONSTRAINT [FK_Member_User]
+GO
+
