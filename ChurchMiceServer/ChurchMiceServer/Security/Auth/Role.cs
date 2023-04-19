7a7278fd51776f428479ba25d0865ab7da9a7223
Chris Laforet <laforet@chrislaforetsoftware.com>
2023-04-18 20:33:03 -0400

    Adding user roles

---
+++ ChurchMiceServer/ChurchMiceServer/Security/Auth/Role.cs
@@ -0,0 +1,58 @@
+﻿namespace ChurchMiceServer.Security.Auth;
+
+public class Role
+{
+	public int Level { get; private set; }
+	public string Name { get; private set; }
+	public bool HasAccess { get; private set; }
+	public bool IsMember { get; private set; }
+	public bool IsAdministrator { get; private set; }
+
+	public static Role GetNoAccess()
+	{
+		return new Role()
+		{
+			Level = 0,
+			Name = "NoAccess",
+			HasAccess = false,
+			IsMember = false,
+			IsAdministrator = false
+		};
+	}
+	
+	public static Role GetAttender()
+	{
+		return new Role()
+		{
+			Level = 100,
+			Name = "Attender",
+			HasAccess = true,
+			IsMember = false,
+			IsAdministrator = false
+		};
+	}
+	
+	public static Role GetMember()
+	{
+		return new Role()
+		{
+			Level = 1000,
+			Name = "Member",
+			HasAccess = true,
+			IsMember = true,
+			IsAdministrator = false
+		};
+	}
+	
+	public static Role GetAdministrator()
+	{
+		return new Role()
+		{
+			Level = 100,
+			Name = "Administrator",
+			HasAccess = true,
+			IsMember = true,
+			IsAdministrator = true
+		};
+	}
+}
---
+++ ChurchMiceServer/ChurchMiceServer/Security/Auth/Roles.cs
@@ -0,0 +1,19 @@
+﻿namespace ChurchMiceServer.Security.Auth;
+
+public class Roles
+{
+	private readonly Role[] roles = { Role.GetNoAccess(), Role.GetAttender(), Role.GetMember(), Role.GetAdministrator() };
+	
+	public Role[] GetRoles() {
+	    return roles;
+	}
+	
+	public Role GetRoleByLevel(int level) {
+	    foreach (var role in roles) {
+	        if (role.Level == level) {
+	            return role;
+	        }
+	    }
+	    return Role.GetNoAccess();
+	}
+}
