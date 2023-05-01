266d1037e992825426a8a24b87d20bcca86aea11
Chris <laforet@chrislaforetsoftware.com>
2023-05-01 16:09:58 -0400

    create member service call

---
+++ ChurchMiceServer/ChurchMiceServer/CQS/CommandHandlers/CreateMemberCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using ChurchMiceServer.CQS.Commands;
+using ChurchMiceServer.CQS.Responses;
+using ChurchMiceServer.Domains.Models;
+using ChurchMiceServer.Domains.Proxies;
+
+namespace ChurchMiceServer.CQS.CommandHandlers;
+
+public class CreateMemberCommandHandler : ICommandHandler<CreateMemberCommand, MemberResponse>
+{
+    private readonly IMemberProxy memberProxy;
+    private readonly ILogger<CreateMemberCommandHandler> logger;
+    
+    public CreateMemberCommandHandler(IMemberProxy memberProxy, ILogger<CreateMemberCommandHandler> logger)
+    {
+        this.memberProxy = memberProxy;
+        this.logger = logger;
+    }
+    
+    public MemberResponse Handle(CreateMemberCommand command)
+    {
+        try
+        {
+            var member = CreateMember(command); 
+            memberProxy.CreateMember(member);
+            logger.Log(LogLevel.Information, string.Format("Created member for %s %s for %s", member.FirstName, member.LastName, ""));
+            return new MemberResponse(member);
+        }
+        catch (Exception ex)
+        {
+            logger.Log(LogLevel.Error, "Error creating member", ex);
+        }
+
+        return null;
+    }
+
+    private static Member CreateMember(CreateMemberCommand command)
+    {
+        var member = new Member();
+        member.FirstName = command.FirstName;
+        member.LastName = command.LastName;
+        member.Email = command.Email;
+        member.HomePhone = command.HomePhone;
+        member.MobilePhone = command.MobilePhone;
+        member.MailingAddress1 = command.MailingAddress1;
+        member.MailingAddress2 = command.MailingAddress2;
+        member.City = command.City;
+        member.State = command.State;
+        member.Zip = command.Zip;
+        member.Anniversary = command.Anniversary;
+        member.Birthday = command.Birthday;
+        //member.UserId
+        member.Created = new DateTime();
+        return member;
+    }
+}
--- ChurchMiceServer/ChurchMiceServer/Controllers/MemberController.cs
+++ ChurchMiceServer/ChurchMiceServer/Controllers/MemberController.cs
@@ -1,4 +1,6 @@
-﻿using ChurchMiceServer.CQS.Queries;
+﻿using ChurchMiceServer.Controllers.Models;
+using ChurchMiceServer.CQS.CommandHandlers;
+using ChurchMiceServer.CQS.Queries;
 using ChurchMiceServer.CQS.QueryHandlers;
 using ChurchMiceServer.CQS.Responses;
 using ChurchMiceServer.Domains.Proxies;
@@ -9,12 +11,13 @@ namespace ChurchMiceServer.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class MemberController
+public class MemberController : ControllerBase
 {
     private readonly IMemberProxy memberProxy;
     private readonly ILogger<UserController> logger;
     
     private readonly MemberQueryHandler memberQueryHandler;
+    private readonly CreateMemberCommandHandler createMemberCommandHandler;
     
     public MemberController(IServiceProvider serviceProvider, IMemberProxy userProxy, ILogger<UserController> logger)
     {
@@ -22,6 +25,7 @@ public class MemberController
         this.logger = logger;
         
         this.memberQueryHandler = ActivatorUtilities.CreateInstance<MemberQueryHandler>(serviceProvider);
+        this.createMemberCommandHandler = ActivatorUtilities.CreateInstance<CreateMemberCommandHandler>(serviceProvider);
     }
 
     [HttpGet("getMemberById")]
@@ -44,6 +48,8 @@ public class MemberController
     [Authorize]
     public MemberResponse CreateMember(MemberRequest request)
     {
-        
+        var identity = this;
+        return null;
+        //return createMemberCommandHandler.Handle(new CreateMemberCommand(request));
     }
 }
