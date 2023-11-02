959f5347221c06874c7298d77caa8cf639053691
Chris <laforet@chrislaforetsoftware.com>
2023-11-02 18:43:59 -0400

    Starting developing a datatype

---
+++ ChurchMiceServer/ChurchMiceServer/DataTypes/UserId.cs
@@ -0,0 +1,25 @@
+﻿namespace ChurchMiceServer.DataTypes;
+
+public class UserId
+{
+    private string userId;
+
+    public UserId(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+        {
+            throw new ArgumentException("A userId cannot be null or empty");
+        }
+        userId = id;
+    }
+
+    public UserId(UserId that)
+    {
+        userId = that.userId;
+    }
+
+    public override string ToString()
+    {
+        return userId;
+    }
+}
